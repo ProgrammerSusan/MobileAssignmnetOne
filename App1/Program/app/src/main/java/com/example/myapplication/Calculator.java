2a13b3750abb5bc8dbadc2e9e12d964caaaf1360
ProgrammerSusan <hatems2017@gmail.com>
2020-01-15 18:04:53 -0500

    created calculator class

---
+++ App1/Program/app/src/main/java/com/example/myapplication/Calculator.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+public class Calculator {
+
+    public float add(float a, float b) {
+        return a + b;
+    }
+
+    public float subtract(float a, float b) {
+        return a - b;
+    }
+
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+
+    public float divide(float a, float b) {
+        return a / b;
+    }
+}
