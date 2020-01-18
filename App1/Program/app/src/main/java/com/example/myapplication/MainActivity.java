aea1fee6114d078b14c768e86eb1ce2886f28bec
ProgrammerSusan <hatems2017@gmail.com>
2020-01-18 17:02:41 -0500

    added functionality

--- App1/Program/app/src/main/java/com/example/myapplication/MainActivity.java
+++ App1/Program/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +15,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void add(View v){
+        EditText firstNumberText = (EditText)findViewById(R.id.firstInput);
+        EditText secondNumberText = (EditText)findViewById(R.id.secondInput);
 
+        String firstInput = firstNumberText.getText().toString();
+        String secondInput = secondNumberText.getText().toString();
+
+        float first = Float.parseFloat(firstInput);
+        float second = Float.parseFloat(secondInput);
+
+        float answer = first + second;
+        TextView results = (TextView)findViewById(R.id.answer);
+        results.setText(answer+"");
     }
 
     public void subtract(View v){
+        EditText firstNumberText = (EditText)findViewById(R.id.firstInput);
+        EditText secondNumberText = (EditText)findViewById(R.id.secondInput);
 
+        String firstInput = firstNumberText.getText().toString();
+        String secondInput = secondNumberText.getText().toString();
+
+        float first = Float.parseFloat(firstInput);
+        float second = Float.parseFloat(secondInput);
+
+        float answer = first - second;
+        TextView results = (TextView)findViewById(R.id.answer);
+        results.setText(answer+"");
     }
 
     public void multiply(View v){
+        EditText firstNumberText = (EditText)findViewById(R.id.firstInput);
+        EditText secondNumberText = (EditText)findViewById(R.id.secondInput);
 
+        String firstInput = firstNumberText.getText().toString();
+        String secondInput = secondNumberText.getText().toString();
+
+        float first = Float.parseFloat(firstInput);
+        float second = Float.parseFloat(secondInput);
+
+        float answer = first * second;
+        TextView results = (TextView)findViewById(R.id.answer);
+        results.setText(answer+"");
     }
 
     public void divide(View v){
+        EditText firstNumberText = (EditText)findViewById(R.id.firstInput);
+        EditText secondNumberText = (EditText)findViewById(R.id.secondInput);
 
+        String firstInput = firstNumberText.getText().toString();
+        String secondInput = secondNumberText.getText().toString();
+
+        float first = Float.parseFloat(firstInput);
+        float second = Float.parseFloat(secondInput);
+
+        float answer = first / second;
+        TextView results = (TextView)findViewById(R.id.answer);
+        results.setText(answer+"");
     }
 }
