df2ef76d817dc1647f29b2a1ad99208607535e96
ProgrammerSusan <hatems2017@gmail.com>
2020-01-18 16:27:21 -0500

    fixed layout and added buttons

--- App1/Program/app/src/main/java/com/example/myapplication/MainActivity.java
+++ App1/Program/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(View v){
+
+    }
+
+    public void subtract(View v){
+
+    }
+
+    public void multiply(View v){
+
+    }
+
+    public void divide(View v){
+
     }
 }
