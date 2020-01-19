3100a464515989e4d9ad4997d5d73c7806c6ed58
ProgrammerSusan <hatems2017@gmail.com>
2020-01-18 21:04:08 -0500

    created function for creating password

--- App2/Program/app/src/main/java/com/example/myapplication/PasswordGenerator.java
+++ App2/Program/app/src/main/java/com/example/myapplication/PasswordGenerator.java
@@ -22,4 +22,17 @@ public class PasswordGenerator {
     public String getBirthday(){
         return this.birthday;
     }
+
+    public String GeneratePassword(){
+        char firstInitial = this.firstName.charAt(0);
+        char lastInitial = this.lastName.charAt(this.lastName.length()-1);
+        String month = this.birthday.substring(0,3);
+        String year = this.birthday.substring(7);
+        String last = this.lastName.substring(0,4);
+        int firstLength = this.firstName.length();
+        int lastLength = this.lastName.length();
+        
+        String password = firstInitial + lastInitial + month + year + last + firstLength + lastLength;
+        return password;
+    }
 }
