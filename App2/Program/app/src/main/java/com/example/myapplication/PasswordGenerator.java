3a962c883ca93029c1f4fcb4630c48989a05c079
ProgrammerSusan <hatems2017@gmail.com>
2020-01-18 20:50:52 -0500

    created class structure

--- App2/Program/app/src/main/java/com/example/myapplication/PasswordGenerator.java
+++ App2/Program/app/src/main/java/com/example/myapplication/PasswordGenerator.java
@@ -1,4 +1,25 @@
 package com.example.myapplication;
 
 public class PasswordGenerator {
+    private String firstName;
+    private String lastName;
+    private String birthday;
+
+    public PasswordGenerator(String first, String last, String birth){
+        this.firstName = first;
+        this.lastName = last;
+        this.birthday = birth;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getBirthday(){
+        return this.birthday;
+    }
 }
