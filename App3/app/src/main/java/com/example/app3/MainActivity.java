c017d65ca46844d25619bfe704aa8f3dcfb0ef11
ProgrammerSusan <hatems2017@gmail.com>
2020-01-20 18:19:25 -0500

    added functionality to UI

--- App3/app/src/main/java/com/example/app3/MainActivity.java
+++ App3/app/src/main/java/com/example/app3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.app3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View v){
+        EditText principal = (EditText)findViewById(R.id.principalInput);
+        EditText addition = (EditText)findViewById(R.id.annualInput);
+        EditText rate = (EditText)findViewById(R.id.returnInput);
+        EditText year = (EditText)findViewById(R.id.yearsInput);
+
+        float current = Float.valueOf(principal.getText().toString());
+        float annual = Float.valueOf(addition.getText().toString());
+        float yearlyRate = Float.valueOf(rate.getText().toString());
+        float yearsOfInvestment = Float.valueOf(year.getText().toString());
+
+        RetirementCalculator userSavings = new RetirementCalculator(current, annual, yearlyRate, yearsOfInvestment);
+
+        int total = userSavings.calculateRetirement();
+
+        TextView displayTotal = (TextView)findViewById(R.id.totalOutput);
+        displayTotal.setText(total+"");
     }
 }
--- App3/app/src/main/java/com/example/app3/RetirementCalculator.java
+++ App3/app/src/main/java/com/example/app3/RetirementCalculator.java
@@ -10,12 +10,12 @@ public class RetirementCalculator {
     public RetirementCalculator(float currentPrincipal, float annualAddition, float rate,  float years){
         this.currentPrincipal = currentPrincipal;
         this.annualAddition = annualAddition;
-        this.rate = rate;
+        this.rate = rate/100;
         this.years = years;
     }
 
-    public float calculateRetirement(){
-        return (currentPrincipal + 100 * annualAddition/rate) * (float)Math.pow(1 + rate/100, years)
-                - 100 * annualAddition/rate;
+    public int calculateRetirement(){
+        float retirement = (currentPrincipal + 100 * annualAddition/rate) * (float)Math.pow(1 + rate/100, years) - 100 * annualAddition/rate;
+        return Math.round(retirement);
     }
 }
