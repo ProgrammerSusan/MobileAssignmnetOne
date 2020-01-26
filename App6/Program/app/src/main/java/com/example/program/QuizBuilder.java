d2617265cf9cbde06c1da01a3337a541bb776d80
ProgrammerSusan <hatems2017@gmail.com>
2020-01-25 20:59:47 -0500

    added more to layout and created class

---
+++ App6/Program/app/src/main/java/com/example/program/QuizBuilder.java
@@ -0,0 +1,9 @@
+package com.example.program;
+
+public class QuizBuilder {
+    private int numberOne;
+    private int numberTwo;
+    private int answer;
+    private int totalQuestions;
+    private int totalCorrect;
+}
